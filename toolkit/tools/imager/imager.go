d6a50bc64def49d8e8c131021bb5910b03e8a3da
andy.peng <andypeng@pglgull002.png.intel.com>
2025-09-11 11:57:53 +0800

    ...

--- toolkit/tools/imager/imager.go
+++ toolkit/tools/imager/imager.go
@@ -126,8 +126,9 @@ func main() {
 	systemConfig.KernelCommandLine.ExtraCommandLine = fmt.Sprintf("%s %s",
 		systemConfig.KernelCommandLine.ExtraCommandLine,
 		"rd.debug rd.udev.log_level=debug rd.timeout=0 systemd.log_level=debug systemd.log_target=kmsg log_buf_len=1M printk.devkmsg=on systemd.debug-shell=1 rd.memdebug=5 rd.live.debug=1 loglevel=7 intel_pstate=disable")
-	panic("oops")
-
+	if *liveInstallFlag {
+		panic("oops")
+	}
 	err = buildSystemConfig(systemConfig, config.Disks, *outputDir, *buildDir, *imgContentFile)
 	logger.PanicOnError(err, "Failed to build system configuration")
 }
